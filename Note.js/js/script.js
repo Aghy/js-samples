6369215fb96e4453fb9de96721020810a8686fc9
Ahmad2 <ahmad@newsletter2go.com>
2017-11-17 16:36:22 +0100

    get and post notes to and from the api

--- Note.js/js/script.js
+++ Note.js/js/script.js
@@ -1,9 +1,24 @@
 $('document').ready(function () {
-    $.get('notes.json').done(function(response){
-        var notes = response.notes;
-        for (var noteIndex in notes) {
-            var listItem = "<li>" + notes[noteIndex].content + "</li>";
-            $('#list').append(listItem);
-        }
+
+    $('#submit').on("click", function () {
+        var textBoxContent = $('#textBox').val();
+        sendNote(textBoxContent);
     });
+
+    $.get('http://notee.de/spaces/test/index.php').done(
+        function listIt(fileContent) {
+            var items = JSON.parse(fileContent);
+            for (var index in items.notes) {
+
+                $('#list').append('<li>' +  items.notes[index].content + '</li>');
+            }
+        }
+    );
+
+
+
 });
+
+function sendNote(note) {
+    $.post('http://notee.de/spaces/test/index.php',{content: note} );
+}
